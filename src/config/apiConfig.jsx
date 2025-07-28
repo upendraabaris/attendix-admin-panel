881ec7d0cb552f0c6daf4be30cc25e04c5a80e62
kritin joshi <kritin.joshi.5@gmail.com>
2025-07-28 15:44:05 +0530

    url

--- src/config/apiConfig.jsx
+++ src/config/apiConfig.jsx
@@ -1,7 +1,7 @@
 // const BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api";
 
-const BASE_URL = "http://localhost:4000/api";
+//const BASE_URL = "http://localhost:4000/api";
 
-//const BASE_URL = "http://shprod.platinum-infotech.com:4000/api";
+const BASE_URL = "http://shprod.platinum-infotech.com:4000/api";
 
 export default BASE_URL;
